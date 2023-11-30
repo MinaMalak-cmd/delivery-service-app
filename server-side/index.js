395857fec9cbe198bdc8fcb30df339e8b86ed190
MinaMalak-cmd <engmina15@yahoo.com>
2023-11-30 15:00:34 +0200

    Initialize App

--- server-side/index.js
+++ server-side/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import path from 'path';
-import initApp from './src/initApp';
+import initApp from './src/initApp.js';
 
 // dotenv.config({ path : path.resolve('./config/.env') });
 
--- server-side/src/initApp.js
+++ server-side/src/initApp.js
@@ -1,7 +1,9 @@
+import userRouter from "./modules/user/user.router.js";
+
 const initApp = async (express) => {
   const app = express();
   const port = process.env.PORT || 5000;
-  app.use(`/`, res.send("Hello World"));
+  app.use(`/user`, userRouter);
   app.use("*", (req, res, next) => {
     return res.json({ message: "In-valid Routing" });
   });

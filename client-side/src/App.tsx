475561faea791bda1aaf38f18cdd9b10db5a300e
MinaMalak-cmd <engmina15@yahoo.com>
2023-12-02 20:09:17 +0200

    call login in App component

--- client-side/src/App.tsx
+++ client-side/src/App.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
 import './App.css';
 import Login from './Views/Login';
+// import SenderDashboard from './Views/SenderDashboard/SenderDashboard';
 
 function App() {
   return (
     <div className="App">
       <Login />
+      {/* <SenderDashboard /> */}
     </div>
   );
 }

23315afaeee8a329cb19f0513f7ad41d70326220
MinaMalak-cmd <engmina15@yahoo.com>
2023-12-05 18:54:11 +0200

    create interfaces

---
+++ client-side/src/Utils/interfaces.ts
@@ -0,0 +1,19 @@
+export interface IUser {
+    _id: string;
+    userName: string;
+    role?: "user" | "biker";
+}
+
+export interface IParcel {
+  _id: string;
+  createdBy: IUser;
+  parcelName: string;
+  pickupAddress: string;
+  dropOffAddress: string;
+  parcelStatus: "picked" | "pending" | "delivered";
+  createdAt: Date;
+  updatedAt: Date;
+  deliveredBy?: IUser;
+  dropOffTime?: string;
+  pickupTime?: string;
+}

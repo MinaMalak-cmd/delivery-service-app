495c09b3993f3395e4f326995b91fd2d52169c63
MinaMalak-cmd <engmina15@yahoo.com>
2023-12-05 18:15:27 +0200

    remove refresh token, add test endpoint

--- server-side/src/modules/user/controllers/auth.contoller.js
+++ server-side/src/modules/user/controllers/auth.contoller.js
@@ -12,18 +12,29 @@ export const login = asyncHandler(async (req, res, next) => {
       return next(new Error("Either Email or Password is wrong", { cause: 404 }));
     }
     const token = jwt.sign({ id: user._id, role: user.role }, process.env.TOKEN_SIGNATURE, {
-      expiresIn: "30m",
+      expiresIn: "3d",
     });
-    const refreshToken = jwt.sign({ id: user._id, role: user.role }, process.env.TOKEN_SIGNATURE, {
-      expiresIn: "30d",
-    });
+    // const refreshToken = jwt.sign({ id: user._id, role: user.role }, process.env.TOKEN_SIGNATURE, {
+    //   expiresIn: "30d",
+    // });
     return SuccessResponse(
       res,
       {
         message: `login success`,
         accessToken: token,
-        refreshToken: refreshToken,
+        // refreshToken: refreshToken,
         user
+      },
+      200
+    );
+  });
+
+  export const getAllUsers = asyncHandler(async (req, res, next) => {
+   const users = await userModel.find();
+    return SuccessResponse(
+      res,
+      {
+        users
       },
       200
     );
--- server-side/src/modules/user/user.router.js
+++ server-side/src/modules/user/user.router.js
@@ -5,5 +5,6 @@ import * as authContoller from "./controllers/auth.contoller.js";
 const router = Router();
 
 router.post("/login", authContoller.login);
+router.get("/get-all", authContoller.getAllUsers);
 
 export default router;

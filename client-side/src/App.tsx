7f9e077dbdbedf2fb628a746a01f853ed6d47c3b
MinaMalak-cmd <engmina15@yahoo.com>
2023-12-06 21:08:51 +0200

    Handle routing

--- client-side/src/App.tsx
+++ client-side/src/App.tsx
@@ -1,13 +1,36 @@
-import './App.css';
-import BikerTool from './Views/BikerTool/BikerTool';
-import Login from './Views/Login/Login';
-import SenderDashboard from './Views/SenderDashboard/SenderDashboard';
+import React from "react";
+import { HashRouter, Route, Routes, Navigate } from "react-router-dom";
 
-function App() {
+import "./App.css";
+import ScrollToTop from "./Components/ScrollToTop/ScrollToTop";
+import Loading from "./Components/Loading/Loading";
+
+const Login = React.lazy(() => import('./Views/Login/Login'));
+const SenderDashboard = React.lazy(() => import('./Views/SenderDashboard/SenderDashboard'));
+const BikerTool = React.lazy(() => import('./Views/BikerTool/BikerTool'));
+
+const App =() => {
   return (
-    // <SenderDashboard />
-    // <BikerTool />
-    <Login />
+    <HashRouter>
+      <React.Suspense fallback={<Loading />}>
+        <ScrollToTop />
+        <Routes>
+          <Route
+            path="/"
+            element={true ? <Navigate to="/login" replace /> : <Login />}
+          />
+          <Route
+            path="/sender-dashboard"
+            element={true ? <SenderDashboard /> : <Navigate to="/" replace />}
+          />
+          <Route
+            path="/biker-tool"
+            element={true ? <BikerTool /> : <Navigate to="/" replace />}
+          />
+          <Route path="*" element={<Login />} />
+        </Routes>
+      </React.Suspense>
+    </HashRouter>
   );
 }
 
---
+++ client-side/src/Components/Loading/Loading.tsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+  return (
+    <div className="loading-wrapper">
+        <div className="spinner"></div>
+    </div>
+  );
+};
+
+export default Loading;

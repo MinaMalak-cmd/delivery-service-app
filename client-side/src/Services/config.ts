6816722e25bd19028244e2e279c13aa685b508fa
MinaMalak-cmd <engmina15@yahoo.com>
2023-12-07 14:15:44 +0200

    use env variables in config file

--- client-side/src/Services/config.ts
+++ client-side/src/Services/config.ts
@@ -1,6 +1,4 @@
 import axios from 'axios';
-
 export const BASE_API = axios.create({
-    // baseURL: getEnv().API_URL
-    baseURL: "http://localhost:5000"
+    baseURL: process.env.REACT_APP_API_URL
   });
